9bfd3a4ef2b48874909b6e2b39fe9f73ab230815
Issa Jaguraga <issa.jaguraga@oclock.school>
2025-06-23 10:01:52 +0000

    chore(env): add devcontainer config and env setup script

---
+++ set_env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export GOOGLE_APPLICATION_CREDENTIALS="$HOME/Documents/TitreProDevops/devops_projects/Luxe-Haven/infra/terraform-service-account.json"
+echo "✅ GOOGLE_APPLICATION_CREDENTIALS variable set."
+
