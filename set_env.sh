2934213f9ca6345cbacd6cf964511d1e59d96138
Issa Jaguraga <issa.jaguraga@oclock.school>
2025-06-24 11:24:28 +0300

    feat(infra): setup GCP with Ubuntu VM, VPC and firewall rules

--- set_env.sh
+++ set_env.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-export GOOGLE_APPLICATION_CREDENTIALS="$HOME/Documents/TitreProDevops/devops_projects/Luxe-Haven/infra/terraform-service-account.json"
-echo "✅ GOOGLE_APPLICATION_CREDENTIALS variable set."
+
+# Récupère le chemin absolu du dossier contenant ce script
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CREDENTIAL_PATH="$SCRIPT_DIR/infra/terraform-service-account.json"
+
+# Vérifie si le fichier existe
+if [ -f "$CREDENTIAL_PATH" ]; then
+  export GOOGLE_APPLICATION_CREDENTIALS="$CREDENTIAL_PATH"
+  echo "✅ Variable GOOGLE_APPLICATION_CREDENTIALS définie : $GOOGLE_APPLICATION_CREDENTIALS"
+else
+  echo "❌ Fichier d'identifiants non trouvé à : $CREDENTIAL_PATH"
+fi
 
